254912a5e9ef14646eed9f0220b1a1c043ae2841
chayanon <chayanoon747@gmail.com>
2024-02-05 23:54:06 +0700

    Update newScreen.js

--- src/newScreen.js
+++ src/newScreen.js
@@ -1,6 +1,8 @@
 import { View } from "react-native"
 
 export const newScreen = ()=>{
+    const feature = "yes"
+
     return(
         <View style={{flex:1}}>
             <Text>รักเบบี๋ที่สุดใน</Text>
