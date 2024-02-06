19dc8dbb983380b87cb5b69c566e42a4cbd96402
chayanon <chayanoon747@gmail.com>
2024-02-06 23:08:32 +0700

    Update newScreen.js

--- src/newScreen.js
+++ src/newScreen.js
@@ -3,6 +3,7 @@ import { View } from "react-native"
 export const newScreen = ()=>{
     const feature = 0
     const featureAdd = 0
+    const featureAdd2 = 0
 
     return(
         <View style={{flex:1}}>
