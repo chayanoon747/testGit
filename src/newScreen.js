08cf75f488c0bb309c0513a94138939afa672d10
chayanon <chayanoon747@gmail.com>
2024-02-06 23:23:17 +0700

    test push

--- src/newScreen.js
+++ src/newScreen.js
@@ -1,13 +1,10 @@
 import { View } from "react-native"
 
 export const newScreen = ()=>{
-    
-    const featureAdd = 0
-    const featureAdd2 = 0
 
     return(
         <View style={{flex:1}}>
-            <Text>test feature  </Text>
+            <Text>test push feature 1  </Text>
         </View>
     )
 }
